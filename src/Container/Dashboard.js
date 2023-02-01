7abc02349075aa616bf1345c65041991adaa2a4d
poojakumari11228 <poojakumari11228@gmail.com>
2023-02-01 04:28:29 -0600

    fetch and update data - props & state

--- src/Container/Dashboard.js
+++ src/Container/Dashboard.js
@@ -1,37 +1,35 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Posts from "../Component/Posts";
 
-function Dashboard(){
+function Dashboard() {
 
 
-    // const {postsState, setPostsState} = useState([
-    //     {id: 1, title:'OOP', author:'Pooja Kumari'},
-    //     {id: 2, title:'WAA', author:'Pooja Kumari'},
-    //     {id: 3, title:'EA',  author:'Pooja Kumari'}
-    // ]);
-
-    const postsState= [
-        {id: 1, title:'OOP', author:'Pooja Kumari'},
-        {id: 2, title:'WAA', author:'Pooja Kumari'},
-        {id: 3, title:'EA',  author:'Pooja Kumari'}
-    ];
+    const [postsState, setPostsState] = useState([
+        { id: 1, title: 'OOP', author: 'Pooja Kumari' },
+        { id: 2, title: 'WAA', author: 'Pooja Kumari' },
+        { id: 3, title: 'EA', author: 'Pooja Kumari' }
+    ]);
 
 
-   const changeName  = ()=> {
-    console.log("change name");
-    // postsState[0] =  {id: 1, title:'Object oriented', author:'Pooja Kumari'};
-   }
+    const inputRef = useRef();
 
-    
+    const changeName = () => {
+        const val = inputRef.current.value;
+        const newPosts = [...postsState];
+        newPosts[0].title = val;
+        setPostsState(newPosts);
+    }
+
+
     return (
         <div>
-         <h1>Dashboard</h1>   
-         <div className="posts">
-         <Posts posts={postsState}  />
-         </div>
+            <h1>Dashboard</h1>
+            <div className="posts">
+                <Posts posts={postsState} />
+            </div>
 
-         <input  type="text" />
-         <button onClick={changeName} >Change Name </button>
+            <input type="text" ref={inputRef} />
+            <button onClick={changeName} >Change Name </button>
         </div>
     );
 }
